ae35ac4022f3c164ab765f21ad8ee17631400e88
Oliver Kennedy <okennedy@buffalo.edu>
2016-10-22 14:10:09 -0400

    Best-guess cache has better filtering on what rows it needs to cache

--- src/main/scala/mimir/ctables/CTAnalyzer.scala
+++ src/main/scala/mimir/ctables/CTAnalyzer.scala
@@ -99,6 +99,72 @@ object CTAnalyzer {
     }
   }
 
+  /**
+   * Find all VGTerms that appear in the expression, and compute a set of conditions
+   * under which each of those terms affect the result.  Similar to compileDeterministic,
+   * but on a term-by-term basis.
+   */
+  def compileCausality(expr: Expression): List[(Expression, VGTerm)] =
+    compileCausality(expr, BoolPrimitive(true))
+
+  private def compileCausality(expr: Expression, inputCondition: Expression): List[(Expression, VGTerm)] = 
+  {
+    expr match { 
+      case Conditional(condition, thenClause, elseClause) => {
+
+        val conditionCausality = compileCausality(condition, inputCondition)
+
+        val thenElseCondition = 
+          if(CTables.isDeterministic(condition)){ 
+            ExpressionUtils.makeAnd(inputCondition, condition)
+          } else {
+            inputCondition
+          }
+
+        conditionCausality ++ 
+          compileCausality(thenClause, thenElseCondition) ++
+          compileCausality(elseClause, thenElseCondition)
+      }
+
+      case Arithmetic(Arith.And, l, r) => {
+        (CTables.isDeterministic(l), CTables.isDeterministic(r)) match {
+          case (true, true)   => List()
+          case (false, true)  => 
+            compileCausality(l, 
+              ExpressionUtils.makeAnd(inputCondition, ExpressionUtils.makeNot(r))
+            )
+          case (true, false)  => 
+            compileCausality(r, 
+              ExpressionUtils.makeAnd(inputCondition, ExpressionUtils.makeNot(l))
+            )
+          case (false, false) => 
+            compileCausality(l, inputCondition) ++ compileCausality(r, inputCondition)
+        }
+      }
+
+      case Arithmetic(Arith.Or, l, r) => {
+        (CTables.isDeterministic(l), CTables.isDeterministic(r)) match {
+          case (true, true)   => List()
+          case (false, true)  => 
+            compileCausality(l, 
+              ExpressionUtils.makeAnd(inputCondition, r)
+            )
+          case (true, false)  => 
+            compileCausality(r, 
+              ExpressionUtils.makeAnd(inputCondition, l)
+            )
+          case (false, false) => 
+            compileCausality(l, inputCondition) ++ compileCausality(r, inputCondition)
+        }
+      }
+
+      case x: VGTerm => List( (inputCondition, x) )
+
+      case _ => expr.children.flatMap(compileCausality(_, inputCondition))
+
+    }
+  }
+
   def compileSample(expr: Expression, seed: Expression): Expression =
   {
     expr match {
