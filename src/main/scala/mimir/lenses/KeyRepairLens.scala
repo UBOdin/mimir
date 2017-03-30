d7e82224077d73586f0691b42a4bd3bc661c9e9e
Oliver Kennedy <okennedy@buffalo.edu>
2017-03-30 15:27:16 -0400

    Fastpath optimization

--- src/main/scala/mimir/lenses/KeyRepairLens.scala
+++ src/main/scala/mimir/lenses/KeyRepairLens.scala
@@ -19,6 +19,9 @@ object KeyRepairLens {
     var schema = db.bestGuessSchema(query)
     val schemaMap = schema.toMap
     var scoreCol:Option[String] = None
+    var fastPath:Option[String] = None
+
+    //////////  Parse Arguments  //////////
     val keys: Seq[String] = args.flatMap {
       case Var(col) => {
         if(schemaMap contains col){ Some(col) }
@@ -30,8 +33,14 @@ object KeyRepairLens {
         scoreCol = Some(col.toUpperCase)
         None
       }
+      case Function("FASTPATH", Seq()) => {
+        fastPath = Some("MIMIR_FASTPATH_"+name)
+        None
+      }
       case somethingElse => throw new SQLException(s"Invalid argument ($somethingElse) for KeyRepairLens $name")
     }
+
+    //////////  Assemble Models  //////////
     val values: Seq[(String, Model)] = 
       schema.
       filterNot( (keys ++ scoreCol) contains _._1 ).
@@ -49,9 +58,52 @@ object KeyRepairLens {
         ( col, model )
       }
 
-    (
-      assemble(query, keys, values, scoreCol),
-      values.map(_._2)
+    //////////  Build Fastpath Lookup Cache If Needed  //////////
+    fastPath.foreach { fastPathTable => 
+      buildFastPathCache(
+        db, 
+        query, 
+        keys.map( k => (k, schemaMap(k))), 
+        values.map(_._1), 
+        fastPathTable
+      )
+    }
+
+    //////////  Assemble the query  //////////
+    val lensQuery = 
+      fastPath match {
+        case None => 
+          assemble(query, keys, values, scoreCol)
+        case Some(fastPathTable) => 
+          assembleFastPath(db, query, keys, values, scoreCol, fastPathTable)
+      }
+
+    ( lensQuery, values.map(_._2) )
+  }
+
+  def buildFastPathCache(
+    db: Database,
+    query: Operator,
+    keys: Seq[(String, Type)], 
+    values: Seq[String], 
+    table: String
+  ): Unit =
+  {
+    val tableElements = 
+      keys.map { k => k._1 + " " + k._2 }.mkString(", ") ++
+      values.map { v => v + " int"} +
+      s"PRIMARY KEY (${keys.map(_._1).mkString(",")})"
+
+    db.backend.update(s"CREATE TABLE $table (${ tableElements.mkString(", ") });")
+
+    db.selectInto(table, 
+      Aggregate(
+        keys.map( k => Var(k._1)),
+        values.map { col => 
+          AggFunction("COUNT", true, List(Var(col)), col)
+        },
+        query
+      )
     )
   }
 
@@ -94,6 +146,50 @@ object KeyRepairLens {
         query
       )
     )
+  }
 
+  def assembleFastPath(
+    db: Database,
+    query: Operator,
+    keys: Seq[String],
+    values: Seq[(String, Model)],
+    scoreCol: Option[String], 
+    fastPathTable: String
+  ): Operator =
+  {
+    val rowsWhere = (cond:Expression) => {
+      Select(
+        ExpressionUtils.makeAnd(keys.map { k => 
+          Comparison(Cmp.Eq, Var(s"MIMIR_KR_$k"), Var(k))
+        }),
+        Join(
+          Project(
+            keys.map { k => ProjectArg(s"MIMIR_KR_$k", Var(k)) },
+            Select(cond, db.getTableOperator(fastPathTable))
+          ),
+          query
+        )
+      )}
+    val rowsWithDuplicates =
+      rowsWhere(
+        ExpressionUtils.makeOr(
+          values.map { v => Comparison(Cmp.Neq, Var(v._1), IntPrimitive(1)) }
+        )
+      )
+    val rowsWithoutDuplicates =
+      rowsWhere(
+        ExpressionUtils.makeAnd(
+          values.map { v => Comparison(Cmp.Eq, Var(v._1), IntPrimitive(1)) }
+        )
+      )
+    Union(
+      rowsWithoutDuplicates,
+      assemble(
+        rowsWithDuplicates,
+        keys,
+        values,
+        scoreCol
+      )
+    )
   }
 }
