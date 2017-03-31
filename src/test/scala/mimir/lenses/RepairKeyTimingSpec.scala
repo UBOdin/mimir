ff7e826c31451e84af35dffb8003d13ec22c7bd6
Michael Brachmann <mrb24@buffalo.edu>
2017-03-31 17:02:17 -0400

    Killed 30 minute view materialization hacks

--- src/test/scala/mimir/lenses/RepairKeyTimingSpec.scala
+++ src/test/scala/mimir/lenses/RepairKeyTimingSpec.scala
@@ -21,9 +21,9 @@ object RepairKeyTimingSpec
   def beforeAll =
   {
     ExperimentalOptions.enable("NO-BG-CACHE")
-    // update("DELETE FROM MIMIR_MODEL_OWNERS")
-    // update("DELETE FROM MIMIR_MODELS")
-    // update("DELETE FROM MIMIR_VIEWS")
+     update("DELETE FROM MIMIR_MODEL_OWNERS")
+     update("DELETE FROM MIMIR_MODELS")
+     update("DELETE FROM MIMIR_VIEWS")
   }
 
   val relevantTables = Set(
@@ -132,24 +132,26 @@ object RepairKeyTimingSpec
                  orders_o_orderkey_run_$i ok, orders_o_orderdate_run_$i od,
                  orders_o_shippriority_run_$i os, orders_o_custkey_run_$i ock,
                  lineitem_l_orderkey_run_$i lok, lineitem_l_shipdate_run_$i lsd
-            where cs.mktsegment = 'BUILDING'
-              and cs.tid = cck.tid
-              and cck.custkey = ock.custkey
-              and ok.orderkey = lok.orderkey
-              and od.orderdate > DATE('1995-03-15')
-              and ock.tid = ok.tid and od.tid = ok.tid and os.tid = ok.tid
+            where od.orderdate > DATE('1995-03-15')
               and lsd.shipdate < DATE('1995-03-17')
-              and lsd.tid = lok.tid
+              and cs.mktsegment = 'BUILDING'
+              and lok.tid = lsd.tid
+              and cck.tid = cs.tid
+              and cck.custkey = ock.custkey
+              and ok.tid = ock.tid 
+              and ok.orderkey = lok.orderkey
+              and od.tid = ok.tid 
+              and os.tid = ok.tid
          """, 1976525000l ),
         (s"""
             select liep.extendedprice
             from lineitem_l_extendedprice_run_$i liep, lineitem_l_shipdate_run_$i lisd,
                  lineitem_l_discount_run_$i lidi, lineitem_l_quantity_run_$i liq
             where lisd.shipdate between DATE('1994-01-01') and DATE('1996-01-01')
-              and lidi.tid = lisd.tid
               and lidi.discount between 0.05 and 0.08
-              and liq.tid = lidi.tid
               and liq.quantity < 24
+              and lisd.tid = lidi.tid
+              and liq.tid = lidi.tid
               and liep.tid = liq.tid
          """, 14182710000l ),
         (s"""
@@ -161,7 +163,8 @@ object RepairKeyTimingSpec
                  nation_n_name_run_$i nn1, nation_n_name_run_$i nn2,
                  nation_n_nationkey_run_$i nk1, nation_n_nationkey_run_$i nk2
             where nn2.name='IRAQ' and nn1.name='GERMANY'
-              and cnk.nationkey = nk2.nationkey and snk.nationkey = nk1.nationkey
+              and cnk.nationkey = nk2.nationkey 
+              and snk.nationkey = nk1.nationkey
               and sk.suppkey = lsk.suppkey
               and ok.orderkey = lok.orderkey
               and ck.custkey = ock.custkey
@@ -206,6 +209,9 @@ object RepairKeyTimingSpec
           AS SELECT TID, ${tableAndKeyColumn._3} FROM ${tableAndKeyColumn._2}
         WITH KEY_REPAIR(TID, ENABLE(FAST_PATH))
       """);
+        //val materializeQuery = s"SELECT * FROM ${tableAndKeyColumn._1}_UNMAT"
+        //val oper = db.sql.convert(db.parse(materializeQuery).head.asInstanceOf[Select])
+        //db.selectInto(tableAndKeyColumn._1, oper)
       }
       println(s"Time:${timeForQuery._2} nanoseconds <- RepairKeyLens:${tableAndKeyColumn._1}")
       timeForQuery._2 should be lessThan tableAndKeyColumn._5
