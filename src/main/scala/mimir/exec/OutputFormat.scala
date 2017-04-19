069d7c7bca4cef4daac5c756fe9437e48af2410c
Oliver Kennedy <okennedy@buffalo.edu>
2017-04-19 16:37:24 -0700

    Padding output for column alignment in console view.

--- src/main/scala/mimir/exec/OutputFormat.scala
+++ src/main/scala/mimir/exec/OutputFormat.scala
@@ -1,5 +1,6 @@
 package mimir.exec
 
+import scala.collection.mutable.Buffer
 import org.jline.terminal.Terminal
 import org.jline.utils.{AttributedStyle,AttributedStringBuilder,AttributedString}
 
@@ -44,30 +45,54 @@ class PrettyOutputFormat(terminal: Terminal)
     terminal.writer.write(msg)
     if(msg.length <= 0  || msg.charAt(msg.length - 1) != '\n'){ terminal.writer.write('\n') }
   }
+
+  type ResultRow = (Seq[(String,Boolean)], Boolean)
+
   def print(result: ResultIterator)
   {
-    terminal.writer.write(result.schema.map( _._1 ).mkString(",")+"\n")
-    terminal.writer.write("------\n")
-    result.foreachRow { row => 
-      val currLine = new AttributedStringBuilder(200)
+    val header:Seq[String] = result.schema.map( _._1 )
+    val spacing = scala.collection.mutable.Seq[Int]()++header.map(_.length)
+    var results:Iterable[ResultRow] = 
+      result.mapRows { row => 
+        ((0 until row.numCols).map { i =>
+          val cell = row(i).toString
+          (cell, row.deterministicCol(i))
+        }, row.deterministicRow())
+      }
+    for( (fields, _) <- results) {
+      for(i <- 0 until spacing.size){
+        val fieldString = fields(i)._1.toString
+        if(fieldString.length > spacing(i)){
+          spacing(i) = fieldString.length
+        }
+      }
+    }
+
+    terminal.writer.write(" "+
+      header.zip(spacing).map { case (title, width) =>
+        title.padTo(width, ' ')
+      }.mkString(" | ")+" \n"
+    );
+    terminal.writer.write(
+      spacing.map { width => "".padTo(width+2, '-') }.mkString("+")+"\n"
+    )
+
+    for( (fields, rowDet) <- results ) {
+      var sep = " "
       val lineStyle = 
-        if(row.deterministicRow()){ AttributedStyle.DEFAULT }
+        if(rowDet){ AttributedStyle.DEFAULT }
         else { AttributedStyle.DEFAULT.faint().underline() }
-
-      var sep = ""
-
-      for(i <- (0 until row.numCols)){
+      val currLine = new AttributedStringBuilder(200)
+      for( ((field, fieldDet), width) <- fields.zip(spacing) ){
         currLine.append(sep, lineStyle)
         currLine.append(
-          row(i).toString,
-          if(row.deterministicCol(i)){ lineStyle } 
+          field.toString.padTo(width, ' '),
+          if(fieldDet){ lineStyle } 
             else { lineStyle.foreground(AttributedStyle.RED) }
         )
-        sep = ","
+        sep = " | "
       }
       currLine.append("\n")
-
-      val lineData = 
       terminal.writer.write(
         currLine.toAttributedString.toAnsi(terminal)
       )
