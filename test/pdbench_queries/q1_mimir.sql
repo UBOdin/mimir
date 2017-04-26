ce802138c189d4a48defe00664df004accf2a004
Oliver Kennedy <okennedy@buffalo.edu>
2017-04-26 19:34:28 -0400

    Queries

--- test/pdbench_queries/q1_mimir.sql
+++ test/pdbench_queries/q1_mimir.sql
@@ -26,11 +26,9 @@ FROM
            (SELECT l_orderkey,
                    lineitem_tid
             FROM
-              (SELECT var_id AS C1,
-                      world_id AS W1,
-                      tid AS lineitem_tid,
+              (SELECT tid AS lineitem_tid,
                       orderkey AS l_orderkey
-               FROM lineitem_l_orderkey) AS U1
+               FROM lineitem_l_orderkey_run_1) AS U1
             JOIN
               (SELECT lineitem_tid
                FROM
@@ -38,7 +36,7 @@ FROM
                   FROM
                     (SELECT tid AS lineitem_tid,
                             shipdate AS l_shipdate
-                     FROM lineitem_l_shipdate) AS X1
+                     FROM lineitem_l_shipdate_run_1) AS X1
                   WHERE l_shipdate < '1995-03-17') U) AS U2 USING (lineitem_tid)) AS U1,
 
            (SELECT o_orderdate,
@@ -61,7 +59,7 @@ FROM
                      FROM
                        (SELECT tid AS customer_tid,
                                custkey AS c_custkey
-                        FROM cust_c_custkey) AS U1
+                        FROM cust_c_custkey_run_1) AS U1
                      JOIN
                        (SELECT customer_tid
                         FROM
@@ -69,7 +67,7 @@ FROM
                            FROM
                              (SELECT tid AS customer_tid,
                                      mktsegment AS c_mktsegment
-                              FROM cust_c_mktsegment) AS X2
+                              FROM cust_c_mktsegment_run_1) AS X2
                            WHERE c_mktsegment = 'BUILDING') U) AS U2 USING (customer_tid)) AS U1,
 
                     (SELECT o_custkey,
@@ -78,25 +76,21 @@ FROM
                      FROM
                        (SELECT tid AS orders_tid,
                                custkey AS o_custkey
-                        FROM orders_o_custkey) AS U1
+                        FROM orders_o_custkey_run_1) AS U1
                      JOIN
                        (SELECT *
                         FROM
                           (SELECT tid AS orders_tid,
                                   orderdate AS o_orderdate
-                           FROM orders_o_orderdate) AS X3
+                           FROM orders_o_orderdate_run_1) AS X3
                         WHERE o_orderdate > '1995-03-15') AS U2 USING (orders_tid)) AS U2
                   WHERE U1.c_custkey=U2.o_custkey) U) AS U1
             JOIN
-              (SELECT var_id AS C1,
-                      world_id AS W1,
-                      tid AS orders_tid,
+              (SELECT tid AS orders_tid,
                       orderkey AS o_orderkey
-               FROM orders_o_orderkey) AS U2 USING (orders_tid)) AS U2
+               FROM orders_o_orderkey_run_1) AS U2 USING (orders_tid)) AS U2
          WHERE U1.l_orderkey=U2.o_orderkey) U) AS U1
    JOIN
-     (SELECT var_id AS C1,
-             world_id AS W1,
-             tid AS orders_tid,
+     (SELECT tid AS orders_tid,
              shippriority AS o_shippriority
-      FROM orders_o_shippriority) AS U2 USING (orders_tid))X4;
+      FROM orders_o_shippriority_run_1) AS U2 USING (orders_tid))X4;
