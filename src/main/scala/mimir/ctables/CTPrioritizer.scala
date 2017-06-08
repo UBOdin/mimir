09bc7343d5c9ba869be8cce7ab11e3cce38ed589
Shivang <iamshivang@gmail.com>
2017-06-08 18:32:05 -0400

    Added CTPrioritizer

---
+++ src/main/scala/mimir/ctables/CTPrioritizer.scala
@@ -0,0 +1,20 @@
+package mimir.ctables;
+
+import optimus.optimization._
+
+object Prioritizer {
+
+	implicit val problem = LQProblem(SolverLib.ojalgo)
+
+	val x = MPFloatVar("x", 100, 200)
+	val y = MPFloatVar("y", 80, 170)
+
+	maximize(-2 * x + 5 * y)
+	add(y >:= -x + 200)
+
+	start()
+	println("objective: " + objectiveValue)
+	println("x = " + x.value + "y = " + y.value)
+
+	release()
+}
---
+++ src/test/scala/mimir/ctables/CTPrioritizerSpec.scala
@@ -0,0 +1,16 @@
+ckage mimir.ctables
+
+import java.io._
+import org.specs2.specification._
+import org.specs2.mutable._
+import mimir.test._
+
+object CTPrioritizerSpec extends Specification {
+ def is = s2"""
+
+ This is my first specification
+   it is working                 $ok
+   really working!               $ok
+                                 """
+
+}
