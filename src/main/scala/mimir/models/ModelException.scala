2b73c2ce1f7bf0ba23eec3a65f977ace89f04f89
Oliver <okennedy@buffalo.edu>
2020-01-29 17:44:35 -0500

    Work in progress

--- src/main/scala/mimir/models/ModelException.scala
+++
@@ -1,5 +0,0 @@
-package mimir.models
-
-import mimir.algebra._
-
-case class ModelException(error:String) extends RAException(error)
--- src/main/scala/mimir/models/StatisticalTraits.scala
+++
@@ -1,8 +0,0 @@
-package mimir.models
-
-import mimir.algebra._
-
-trait FiniteDiscreteDomain 
-{
-  def getDomain(idx:Int, args:Seq[PrimitiveValue], hints:Seq[PrimitiveValue]): Seq[(PrimitiveValue,Double)]
-}
